0a6dcf66a32b7637a0ffd7dc94be08c776de0c43
iFruze <denisbyev4s@gmail.com>
2026-03-14 10:48:05 +0300

    fix text on enter room button

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -140,7 +140,7 @@ function App() {
           />
 
           <button onClick={handleJoin} disabled={!isAuth}>
-            Войти
+            Войти в комнату
           </button>
 
           <button onClick={handleCreate} disabled={!isAuth}>
